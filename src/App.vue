87c0f0885b1d4105e2e4f21b5308bc55bf32df4d
肖瑶婕 <1183164969@qq.com>
2026-03-24 20:56:13 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <el-button type="primary">Element Plus 已就绪</el-button>
+    <HelloWorld />
+  </div>
+</template>
